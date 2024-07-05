42456a77d7c73f597ce2b3a9178c8f7f3fc96258
Bdmnss <lukabadali123@gmail.com>
2024-07-05 23:27:19 +0400

    seperate by components

---
+++ src/components/Title.tsx
@@ -0,0 +1,19 @@
+const Title = () => {
+  return (
+    <div className="text-center mb-[6.4rem] lg:w-[45%] lg:text-left 2xl:w-[35%]">
+      <h1
+        className="text-[2.8rem] text-white font-bold leading-[3.6rem] mb-[1.6rem]
+          lg:text-[4rem] lg:leading-[5rem] xl:text-[5rem]"
+      >
+        Learn to code by watching others
+      </h1>
+      <p className="text-[1.6rem] text-white font-medium leading-[2.6rem]">
+        See how experienced developers solve problems in real-time. Watching
+        scripted tutorials is great, but understanding how developers think is
+        invaluable.
+      </p>
+    </div>
+  );
+};
+
+export default Title;
